a97ad1a7c16bc91335877956579fd5d05ea8e115
Nutifafa Foli-Biaku <nutifafa.foli@gmail.com>
2023-02-16 16:14:51 +0000

    print with puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 #include <string.h>
-
+/**
+ * main - Print out lines with put function
+ * Return: 0 (success)
+ */
 int main(void)
 {
-	char str[] = "\"Programming is like building a multilingual puzzle";
-
-	puts(str);
-
-	return (0);
+char str[] = ("\"Programming is like building a multilingual puzzle";)
+puts(str);
+return (0);
 }
