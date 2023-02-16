5a7217e498189bcc093c0e1038d30d5ebfabf25f
Nutifafa Foli-Biaku <nutifafa.foli@gmail.com>
2023-02-16 22:51:24 +0000

    print with fprintf

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - print with fprintf
+ * Return: 1 (success)
+ */
+int main(void)
+{
+	fprintf(stdout, "and that piece of art is useful\"\n");
+	fprintf(stderr,	"- Dora Korpar, 2015-10-19\n");
+	return (1);
+}
