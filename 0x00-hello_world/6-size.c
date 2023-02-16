0691826ee7c85be649a1ffce909cda56c9762ab5
Nutifafa Foli-Biaku <nutifafa.foli@gmail.com>
2023-02-16 18:05:49 +0000

    print size of file types

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,15 +5,15 @@
 */
 int main(void)
 {
-char charType;
-int intType;
-long int longintType;
-long long int longlongintType;
-float floatType;
-printf("Size of a char: %lu byte(s)\n",(unsigned long)sizeof(charType));
-printf("Size of an int: %lu byte(s)\n",(unsigned long)sizeof(intType));
-printf("Size of a long int %lu byte(s)\n",(unsigned long)sizeof(longintType));
-printf("Size of a long long int %lu byte(s)\n",(unsigned long)sizeof(longlongintType));
-printf("Size of a float %lu byte(s)\n",(unsigned long)sizeof(floatType));
+char c;
+int i;
+long int li;
+long long int lli;
+float f;
+printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(c));
+printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(i));
+printf("Size of a long int %lu byte(s)\n", (unsigned long)sizeof(li));
+printf("Size of a long long int %lu byte(s)\n", (unsigned long)sizeof(lli));
+printf("Size of a float %lu byte(s)\n", (unsigned long)sizeof(f));
 return (0);
 }
