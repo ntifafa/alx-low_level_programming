95425bbb03609d8e2e50f0565e5b6926d92e3d33
Nutifafa Foli-Biaku <nutifafa.foli@gmail.com>
2023-02-16 19:24:47 +0000

    print size of files

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,16 +5,12 @@
 */
 int main(void)
 {
-	char c;
-	int i;
-	long int li;
-	long long int lli;
-	float f;
 
-printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(c));
-printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(i));
-printf("Size of a long int %lu byte(s)\n", (unsigned long)sizeof(li));
-printf("Size of a long long int %lu byte(s)\n", (unsigned long)sizeof(lli));
-printf("Size of a float %lu byte(s)\n", (unsigned long)sizeof(f));
+printf("Size of a char: %zu byte(s)\n", sizeof(char));
+printf("Size of an int: %zu byte(s)\n", sizeof(int));
+printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
+printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
+printf("Size of a float: %zu byte(s)\n", sizeof(float));
+
 return (0);
 }
