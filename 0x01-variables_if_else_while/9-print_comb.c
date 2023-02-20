c254380e824b6e1f9234be8eb956b01484022d04
Nutifafa Foli-Biaku <nutifafa.foli@gmail.com>
2023-02-20 21:14:39 +0000

    print possible combinations of single-digit numbers

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Print all possible combinations of single-digit numbers
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	int a;
+
+	for (a = 0; a < 10; a++)
+	{
+		putchar(a + '0');
+		if (a < 99)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
+
