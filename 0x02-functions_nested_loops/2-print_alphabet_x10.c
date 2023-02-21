b1d32ce7c19daaed80512fee5d85be4497272421
Nutifafa Foli-Biaku <nutifafa.foli@gmail.com>
2023-02-21 17:06:00 +0000

    print the alphabets 10x

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - prints the alphabets ten times in lowercase
+ */
+
+void print_alphabet_x10(void)
+{
+	char ch;
+	int i = 0;
+
+	while (i < 10)
+	{
+		ch = 'a';
+		while (ch <= 'z')
+		{
+			_putchar(ch);
+			ch++;
+		}
+		_putchar('\n');
+		i++;
+	}
+}
