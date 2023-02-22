d3091cac36dbe7547eb0d7741bc0127ba5ab7125
Nutifafa Foli-Biaku <nutifafa.foli@gmail.com>
2023-02-22 18:18:22 +0000

    test code with main file

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <time.h>
+#include <math.h>
+#include <stdlib.h>
+
+/**
+ * main - else if statements and print output
+ * Return: 0 (success)
+ */
+
+int positive_or_negative(int i)
+{
+	srand(time(0));
+	i = rand() - RAND_MAX / 2;
+	if (i > 0)
+		printf("%d is positive\n", i);
+	else if (i == 0)
+		printf("%d is zero\n", i);
+	else if (i < 0)
+		printf("%d is negative\n", i);
+	return (0);
+}
