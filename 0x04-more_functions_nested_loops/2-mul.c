832a7447f4e605f3c086115db585df89b50ee4d6
Nutifafa Foli-Biaku <nutifafa.foli@gmail.com>
2023-02-23 22:26:34 +0000

    multiply 2 integers

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -5,13 +5,9 @@
  * Return: c
  * @a: integer to be multiplied
  * @b: integer to be multplie
- * @c: product
  */
 
 int mul(int a, int b)
 {
-	int c;
-
-	c = (a * b);
-	return (c);
+	return (a * b);
 }
