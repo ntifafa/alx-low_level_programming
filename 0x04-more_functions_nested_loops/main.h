3aacf0370e63d7fb550978a6dd37fffe397d8da8
Nutifafa Foli-Biaku <nutifafa.foli@gmail.com>
2023-02-23 17:09:39 +0000

    header file and readme

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,15 @@
+#ifndef main_h
+#define main_h
+
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
+print_numbers(void);
+print_most_numbers(void);
+more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);
+
+#endif
