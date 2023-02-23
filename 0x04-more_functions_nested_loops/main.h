7fdc1d39b7d41baa286c98e2f5e590a6ea7aadc2
Nutifafa Foli-Biaku <nutifafa.foli@gmail.com>
2023-02-23 21:05:56 +0000

    check for uppercase

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -2,14 +2,5 @@
 #define main_h
 
 int _isupper(int c);
-int _isdigit(int c);
-int mul(int a, int b);
-print_numbers(void);
-print_most_numbers(void);
-more_numbers(void);
-void print_line(int n);
-void print_diagonal(int n);
-void print_square(int size);
-void print_triangle(int size);
 
 #endif
