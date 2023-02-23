4aba548ace5566b29d2be3670525cd1c7e0715d4
Nutifafa Foli-Biaku <nutifafa.foli@gmail.com>
2023-02-23 21:22:51 +0000

    check for digit or alpha

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isdigit - check for a digit 
+ * @c: char to be checked
+ * Return: 1 is true and 0 if otherwise
+ */
+
+int _isdigit(int c) 
+{
+	if (c >= '0' && c <= '9')
+		return (1);
+	else 
+		return (0);
+}	
