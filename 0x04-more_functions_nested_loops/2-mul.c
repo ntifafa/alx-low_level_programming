9aa8f0cbbeed61e69ec8a0fc765479a95f89a0dc
Nutifafa Foli-Biaku <nutifafa.foli@gmail.com>
2023-02-23 22:19:37 +0000

    multiply 2 integers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two integers
+ * Return: c
+ * @a: integer to be multiplied
+ * @b: integer to be multplie
+ * @c: product
+ */
+
+int mul(int a, int b)
+{
+	int c;
+
+	c = (a * b);
+	return (c);
+}
