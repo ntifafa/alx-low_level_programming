a255786f58e47d7cc8f807bd502f1e26aedd61c4
Nutifafa Foli-Biaku <nutifafa.foli@gmail.com>
2023-02-24 00:29:33 +0000

    print 0-14 ten times

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,18 +1,18 @@
 #include "main.h"
 
 /**
- * more_numbers - print 10 times the numbers 
- * Return: 0 
+ * more_numbers - print 10 times the numbers
+ * Return: 0
  *
  */
 
 void more_numbers(void)
-{	
+{
 	int a;
 	int b;
-	
+
 	for (a = 1; a <= 14; a++)
-	{	
+	{
 		for (b = 0; b <= 14; b++)
 		{
 			if (b >= 10)
