d178321cdf09b08b82e2e059ab6dcb83336b53b8
Nutifafa Foli-Biaku <nutifafa.foli@gmail.com>
2023-02-27 16:06:38 +0000

    function to change pointer value

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * reset_to_98 - take into as parameter change pointer value to 98
+ *
+ * @n: pointer parameter
+ * Return (void)
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This is to help me learn pointers and arrays
