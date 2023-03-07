fcd48ec617065c18fc63472da15701a382d1e36e
Nutifafa Foli-Biaku <nutifafa.foli@gmail.com>
2023-03-07 17:47:31 +0000

    locates a character in a string

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/*
+ * *_strchr - function locates a character in a string
+ * @c: first occurrence of the string
+ * @s: string to extract @c from
+ * Return: pointer to @c, if not found return NULL
+ */
+
+char *_strchr(char *s, char c)
+{
+	int a;
+
+	for (a = 0; s[a] >= '\0'; a++)
+	{
+		if (s[a] == c)
+			return (&s[a]);
+	}
+	return (0);
+}
+
+
