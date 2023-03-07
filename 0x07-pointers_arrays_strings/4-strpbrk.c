eaa98a671a740cd032e373d74f035b6f45ea17a0
Nutifafa Foli-Biaku <nutifafa.foli@gmail.com>
2023-03-07 20:52:53 +0000

    search string for any set of bytes

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strpbrk - searches a string for any of a set of bytes
+ * @s: the string to search
+ * @accept: the bytes to search for
+ * Return: a pointer to the byte in @s
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	int a;
+
+	while (*s)
+	{
+		for (a = 0; accept[0]; a++)
+		{
+		if (*s == accept[a])
+		return (s);
+		}
+	s++;
+	}
+	return ('\0');
+}
