42a43e52bac0e78f8751a6cedff32a67f7fa6c4e
Nutifafa Foli-Biaku <nutifafa.foli@gmail.com>
2023-03-08 11:26:15 +0000

    update main.h

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -9,6 +9,8 @@ char *_strstr(char *haystack, char *needle);
 void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
 int _putchar(char c);
+char *_strpbrk(char *s, char *accept);
+void set_string(char **s, char *to);
 
 #endif
 
