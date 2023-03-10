b00954c500ae68ffec9f6cd81edc8c24ca457845
Nutifafa Foli-Biaku <nutifafa.foli@gmail.com>
2023-03-10 19:25:42 +0000

    prints all args passed

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints all arguments passed
+ * @argc: counts number of arguments
+ * @argv: arguments
+ * Return: 0 (success)
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+	
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
