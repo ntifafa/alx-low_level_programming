831d042a5c817864e2d27d5e8cc296a299985448
Nutifafa Foli-Biaku <nutifafa.foli@gmail.com>
2023-03-10 18:30:07 +0000

    print name with new line

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - program prints its name followed by new line
+ * @argc: counts cmd line arguments
+ * @argv: arguments
+ * Return: 0 (success)
+ */
+
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+	printf("%s\n", argv[i]);
+	}
+
+	return (0);
+}
