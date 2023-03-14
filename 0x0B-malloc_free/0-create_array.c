4a7e544dec97f47c5e749664b71ce32872df0b06
Nutifafa Foli-Biaku <nutifafa.foli@gmail.com>
2023-03-14 12:37:59 +0000

    allocate memory with placeholder

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * create_array - creates an array and intializes with a specific char
+ * @c: character to be initialized
+ * @size: size of array
+ * Return: a pointer or NULL if it fails or the size is 0
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char *create_array;
+	unsigned int i;
+
+	create_array = malloc(size * sizeof(char));
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+
+	else
+	for (i = 0; i < size; i++)
+	{
+		create_array[i] = c;
+	}
+	return (create_array);
+}
+
