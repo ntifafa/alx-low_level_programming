9be414994824659e10b0e0cd29abf38d5fb9be4f
Nutifafa Foli-Biaku <nutifafa.foli@gmail.com>
2023-03-14 13:01:46 +0000

    allocate memory with placeholder

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -16,14 +16,12 @@ char *create_array(unsigned int size, char c)
 	create_array = malloc(size * sizeof(char));
 
 	if (size == 0)
-	{
 		return (NULL);
-	}
+
 
 	for (i = 0; i < size; i++)
-	{
 		create_array[i] = c;
-	}
+
 	return (create_array);
 }
 
