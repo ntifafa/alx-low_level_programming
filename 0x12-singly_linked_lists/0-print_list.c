0aecde04eb611bf3c4af1f216d6a8f22918c129f
Nutifafa Foli-Biaku <nutifafa.foli@gmail.com>
2023-03-30 20:47:27 +0000

    updated 0-print_list file

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -2,11 +2,8 @@
 
 /**
  * print_list - singly linked list
- * @str: string - (malloc'ed string)
- * @len: length of the string
- * @next: points to the next node
- *
- * Description: singly linked list node structure
+ * @h: pointer to string
+ * Return: number of nodes
  */
 
 size_t print_list(const list_t *h)
