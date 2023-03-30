164ab58ad739815ae1474f6c9212231b01b84faf
Nutifafa Foli-Biaku <nutifafa.foli@gmail.com>
2023-03-30 19:14:02 +0000

    added file 0-print_list.c for task 0

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ */
+
+size_t print_list(const list_t *h)
+{
+/*declare and initialize size_t*/
+	size_t counter = 0;
+/*traverse list_t and print each member*/
+	while (h != NULL)
+{
+	if (h->str == NULL)
+		printf("[0] (nil)\n");
+	else
+		printf("[%u] %s\n", h->len, h->str);
+	h = h->next;
+	counter++;
+}
+	return (counter);
+}
