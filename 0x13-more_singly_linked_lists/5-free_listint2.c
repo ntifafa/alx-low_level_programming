46f62fc9cfe8b8e25bf48f376002b91bdb532730
Nutifafa Foli-Biaku <nutifafa.foli@gmail.com>
2023-04-04 12:34:25 +0000

    updated 5-free_listint2.c file for task 5

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -8,12 +8,21 @@
 
 void free_listint2(listint_t **head)
 {
-	/*declare pointer*/
-	listint_t *now;
+	/*declare pointers*/
+	listint_t *now, *temp;
+	/*verify memory allocation*/
+	if (head == NULL)
+		return;
 	/*point now to memory address of *head*/
 	now = *head;
-	/*free memory allocation*/
-	free(now);
+	/*traverse and free list with loop*/
+	while (now != NULL)
+	{
+		temp = now->next;
+		free(now);
+		now = temp;
+	}
+	/*assign head to NULL*/
 	*head = NULL;
 }
 
