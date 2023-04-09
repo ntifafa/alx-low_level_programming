566d3ec24b31497d85196b19cdd6a7721c1eeb34
Nutifafa Foli-Biaku <nutifafa.foli@gmail.com>
2023-04-09 16:36:42 +0000

    added 0-binary_to_unit file for task 0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * binary_to_unit - convert a binary number to unsigned int
+ * binary_to_uint - convert a binary number to unsigned int
  * @b: pointing to string of 0 and 1 chars
  * Return: converted number, or 0 if there is no 0 or 1 in string
  * or b is NULL
@@ -10,24 +10,25 @@
 unsigned int binary_to_uint(const char *b)
 {
 	/*declare and initialize variables*/
+	unsigned int i =0;
 	unsigned int output = 0;
-	size_t len = strlen(b);
-	size_t i = 0;
-
+	
 	/*check whether string is empty*/
 	if (b == NULL)
 		return (0);
-	
-	/*while loop to iterate through b string values*/
-	while (i < len)
-	{
-		if (b[i] != '0' && b[i] != '1')
-			return (0);
 
+	/*for loop to iterate through b string values*/
+	for (; b[i] != '\0'; i++)
+	{
+		if (b[i] == '0' || b[i] == '1')
+		{
+			/*1 bit left shift, add current num and deduct 0 to convert to ASCII num value*/
+			output = (output << 1) + (b[i] - '0');
+		}
 		else
-		/*bit left shift by 1, add current num and deduct 0 to convert to ASCII num value*/
-		output = (output << 1) + (b[i] - '0');
-		i++;
+		{
+			return (0);
+		}
 	}
 	return (output);
 }
