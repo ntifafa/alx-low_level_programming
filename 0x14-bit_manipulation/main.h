ba6ff4490af0ee220186a429f0006c16f600e1d8
Nutifafa Foli-Biaku <nutifafa.foli@gmail.com>
2023-04-10 10:27:13 +0000

    updated header file

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,5 +1,9 @@
 #ifndef MAIN_H
 #define MAIN_H
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
 
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
