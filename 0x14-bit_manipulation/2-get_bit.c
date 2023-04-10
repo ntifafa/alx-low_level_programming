26018b3b373965954550fbc1427332642f689afc
Nutifafa Foli-Biaku <nutifafa.foli@gmail.com>
2023-04-10 12:47:07 +0000

    updated 2-get_bit.c file for task 2

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -6,8 +6,10 @@
  * @index: bit to get
  * Return: bit at an index, otherwise -1 if an error is encountered
 */
+
 int get_bit(unsigned long int n, unsigned int index)
-{
+{	
+	/*verify if index is within valid range*/
 	if (index >= sizeof(unsigned long int) * 8)
 	{
 		return (-1);
