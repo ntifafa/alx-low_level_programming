98574ff898fdc02c32c22c0f76bcd17bff0748f1
Nutifafa Foli-Biaku <nutifafa.foli@gmail.com>
2023-05-04 19:05:04 +0000

    updated header file

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,5 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
+
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
@@ -12,5 +13,6 @@ int set_bit(unsigned long int *n, unsigned int index);
 int clear_bit(unsigned long int *n, unsigned int index);
 unsigned int flip_bits(unsigned long int n, unsigned long int m);
 int _putchar(char c);
+int get_endianness(void);
 
 #endif
