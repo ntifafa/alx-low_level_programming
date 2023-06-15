a92a83ad31831ef98214f2bc61dbb23d5530b42d
Nutifafa Foli-Biaku <nutifafa.foli@gmail.com>
2023-06-15 20:26:17 +0000

    updated file for task 2

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -12,10 +12,13 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	dlistint_t *new_node = malloc(sizeof(dlistint_t));
 
 	new_node->n = n;
-	new_node->next = NULL;
+    new_node->prev = NULL;
+    new_node->next = *head;
 
-	new_node->next = *head;
-	*head = new_node;
-
-	return (*head);
+    if (*head != NULL) 
+	{
+        (*head)->prev = new_node;
+    }
+    *head = new_node;
+    return (*head);
 }
